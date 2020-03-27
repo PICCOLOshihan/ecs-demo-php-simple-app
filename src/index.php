cb68ab729e5cd64e8bb0c2bba11f260b995f903c
Fikrat <mofikrat@38f9d3dbe64b.ant.amazon.com>
2020-03-27 20:23:31 +0100

    update JWplayer

--- src/index.php
+++ src/index.php
@@ -1,33 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
+<head>
+<script type='text/javascript' src='jwplayer/jwplayer.js'></script>
+</head>
 
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>ECS DEMO</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-        <style>body {margin-top: 40px; background-color: #333;}</style>
-        <link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet">
-        <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-    </head>
-
-    <body>
-        <div class="container">
-            <div class="hero-unit">
-                <h1>Hello Patrick V2</h1>
-                <h2>This is a simple PHP App running on ECS with continuous integration/delivery</h2>
-                <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
-                <?php
-                        $myfile = fopen("/var/www/my-vol/date", "r") or die("");
-                        echo fread($myfile,filesize("/var/www/my-vol/date"));
-                        fclose($myfile);
-                ?>
-            </div>
-        </div>
-
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-        <script src="assets/js/bootstrap.min.js"></script>
-    </body>
-
+<body>
+<div id='container'>Loading the player...</div>
+<script type='text/javascript'>
+jwplayer('container').setup({
+  flashplayer: 'jwplayer/player.swf',
+  file:'http://www.youtube.com/watch?v=bBEUOZ1-_gE',
+  height: 400,
+  width: 600,
+  'playlist.position': 'right',
+  'playlist.size': 80
+  })
+</script>
+</body>
 </html>
